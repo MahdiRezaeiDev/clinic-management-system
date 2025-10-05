031c559847a358491c355e4c728491d6ff92db01
MahdiRezaeiDev <mahdi.sohaily4030@gmail.com>
2025-10-05 22:24:59 +0430

    Added login page background image

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,9 +1,10 @@
+import login_bg from '@/img/login_bg.png';
 export default function GuestLayout({ children }) {
     return (
         <div
             className="bg-blueGray-800 bg-full h-screen bg-no-repeat font-sans"
             style={{
-                backgroundImage: '../../resources/img/register_bg_2.png',
+                backgroundImage: `url(${login_bg})`,
             }}
         >
             <main className="relative w-full pt-32">
