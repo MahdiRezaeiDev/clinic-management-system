041d99cd1df63feceac56b144546646bde6fecf9
MahdiRezaeiDev <mahdi.sohaily4030@gmail.com>
2025-10-09 10:00:49 +0430

    Fixed the sidebar active nav status

--- resources/js/Components/NavLink.jsx
+++ resources/js/Components/NavLink.jsx
@@ -11,7 +11,7 @@ export default function NavLink({
             {...props}
             className={
                 'text-blueGray-800 block px-2 py-3 text-sm font-bold uppercase hover:bg-gray-200' +
-                (active ? 'bg-gray-200' : '') +
+                (active ? ' bg-gray-200' : '') +
                 className
             }
         >
